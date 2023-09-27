06d02d8903f9b5fba9e8a8bfa5ccc7c5aed69de3
Seth Barklow <sethbarklow@gmail.com>
2023-09-27 06:05:54 +0000

    setup speedtest function

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const {exec} = require("child_process");
-exec("speedtest", (error, stdout, stderr) =>{
-    if(error){
-        console.log(`error: ${stderr.message}`);
-        return;
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+async function runSpeedTest(){
+    try{
+        let {stdout, stderr} = await exec("speedtest");
+        
+        let ping = stdout.match(/(?<=Latency:)(.*)(?=\(jitter)/gm)[0];
+        ping = ping.trim();
+
+        let download_speed = stdout.match(/(?<=Download:)(.*)(?=\(data)/gm)[0];
+        download_speed = download_speed.trim();
+        
+        let upload_speed = stdout.match(/(?<=Upload:)(.*)(?=\(data)/gm)[0];
+        upload_speed = upload_speed.trim();
+
+        return [download_speed, upload_speed, ping];
+
+    }catch(e){
+        console.log(e);
     }
-    if(stderr){
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-})
+}
+
+(async ()=>{
+    let inet = await runSpeedTest();
+    console.log(inet);
+})();
+
